9521be5a5e590ac7d6a223d61403e65443dff1a7
nensisukhadiya <nensisukhadiya07@gmail.com>
2025-10-10 16:15:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 print("Jenkins test successful!")
 print("GitHub hook trigger for GITScm polling")
-print("hello nensi")
+
 
 print("Jenkins test successful!")
 print("GitHub hook trigger for GITScm polling")
